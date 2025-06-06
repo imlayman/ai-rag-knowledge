3bb0daeb3ab5200720e5c62f5be7cb669c7ea42c
谢旭峰 <948473561@qq.com>
2025-06-06 18:03:42 +0800

    feat: 第2节：初始化知识库工程 & 提交代码

---
+++ xxf-dev-tech-api/src/main/java/cn/xufstack/xxf/dev/tech/api/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author Xiexufeng @谢旭峰
+ * @description
+ * @create 2025/6/6 17:42
+ */
+package cn.xufstack.xxf.dev.tech.api;
---
+++ xxf-dev-tech-app/src/main/java/cn/xufstack/xxf/dev/tech/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author Xiexufeng @谢旭峰
+ * @description
+ * @create 2025/6/6 17:43
+ */
+package cn.xufstack.xxf.dev.tech;
